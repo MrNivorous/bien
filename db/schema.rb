ccb9f393829b230919a17e3d6b6a6401e22b70fe
MrNivorous <karn.sood@gmail.com>
2018-03-04 21:54:21 -0600

    Added ambiance and SEO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225194853) do
+ActiveRecord::Schema.define(version: 20180305031919) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180225194853) do
     t.string "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
