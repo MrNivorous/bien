99ebdfb73100b3af5e4db687e502a3fbc87ae315
MrNivorous <karn.sood@gmail.com>
2018-04-24 07:18:56 -0500

    Featured homepage reviews

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIYCIRXGC4XQZGZZQ',                        # required
-    aws_secret_access_key: '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',                        # required
+
   }
   config.fog_directory  = 'bien-review'                                   # required
 
